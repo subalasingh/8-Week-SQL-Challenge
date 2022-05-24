93ed39d80ba3098478e9f0706d6c36f73b15428d
Subala Singh <90029373+subalasingh@users.noreply.github.com>
2022-05-24 10:23:43 +0530

    Update Danny's Diner.sql

--- Case Study #1 - Danny's Diner/SQL Syntax/Danny's Diner.sql
+++ Case Study #1 - Danny's Diner/SQL Syntax/Danny's Diner.sql
@@ -68,17 +68,35 @@ VALUES
 SELECT 
  customer_id, 
  SUM(price) As total_amount 
-FROM sales JOIN menu 
+FROM dannys_diner.sales JOIN dannys_diner.menu 
 ON sales.product_id = menu.product_id 
 GROUP BY customer_id;
+
+--Result:
++──────────────+──────────────+
+| customer_id  | total_spent  |
++──────────────+──────────────+
+| A            | 76           |
+| B            | 74           |
+| C            | 36           |
++──────────────+──────────────+            
 
 --2. How many days has each customer visited the restaurant?
 
 SELECT 
  customer_id, 
  COUNT(DISTINCT(order_date)) AS visited_days
-FROM sales
+FROM dannys_diner.sales
 GROUP BY customer_id;
+
+--Result:
++──────────────+───────────────+
+| customer_id  | visited_days  |
++──────────────+───────────────+
+| A            | 4             |
+| B            | 6             |
+| C            | 2             |
++──────────────+───────────────+
 
 --3. What was the first item from the menu purchased by each customer?
 
@@ -92,10 +110,19 @@ WITH c_order As
 	   order_date, 
 	   sales.product_id
   ) AS item_rank  
- FROM sales JOIN menu
+ FROM dannys_diner.sales JOIN dannys_diner.menu
 ON sales.product_id = menu.product_id
 ) 
 Select * FROM c_order WHERE item_rank = 1;
+
+--Result:
++──────────────+───────────────+─────────────+
+| customer_id  | product_name  | item_order  |
++──────────────+───────────────+─────────────+
+| A            | sushi         | 1           |
+| B            | curry         | 1           |
+| C            | ramen         | 1           |
++──────────────+───────────────+─────────────+
 
 --4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
@@ -103,13 +130,18 @@ Select
  product_name,
  COUNT(menu.product_id) AS item_count 
 FROM 
-sales JOIN menu 
+dannys_diner.sales JOIN dannys_diner.menu 
 ON sales.product_id = menu.product_id
 GROUP BY menu.product_name 
 ORDER BY item_count DESC 
 LIMIT 1;
 
+--Result:
++─────────────+───────────────+──────────────+
+| product_id  | product_name  | order_count  |
++─────────────+───────────────+──────────────+
+| 3           | ramen         | 8            |
++─────────────+───────────────+──────────────+
+
 --5. Which item was the most popular for each customer?
 
---------------------------------
---------------------------------
