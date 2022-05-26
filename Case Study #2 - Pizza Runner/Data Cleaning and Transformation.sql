aa53dc935aa6096497d452dea11f0522e1bae439
Subala Singh <90029373+subalasingh@users.noreply.github.com>
2022-05-26 23:16:37 +0530

    Update Data Cleaning and Transformation.sql

--- Case Study #2 - Pizza Runner/Data Cleaning and Transformation.sql
+++ Case Study #2 - Pizza Runner/Data Cleaning and Transformation.sql
@@ -47,8 +47,8 @@ WHERE table_name = 'runner_orders';
 --1. customer_order
 /*
 Cleaning customer_orders
-- Identify records with 'null' values
-- Updating 'null' values to ''
+- Identify records with null or 'null' values
+- Updating null or'null' values to ''
 - Blanks '' indicate that the customer requested no extras/exclusions for the pizza, whereas null values would be ambiguous on this.
 */
 
@@ -92,3 +92,57 @@ SELECT * FROM updated_customer_orders;
 | 10        | 104          | 1         |             |         | 2020-01-11T18:34:49.000Z  |
 | 10        | 104          | 1         | 2, 6        | 1, 4    | 2020-01-11T18:34:49.000Z  |
 +───────────+──────────────+───────────+─────────────+─────────+───────────────────────────+
+
+
+--2. runner_order
+/*
+- pickup time, distance, duration is of the wrong type
+- records have nulls in these columns when the orders are cancelled
+- converted text 'null' to null values
+- removed units (km, minutes) from distance and duration
+*/
+
+
+DROP TABLE IF EXISTS updated_runner_orders;
+CREATE TEMP TABLE updated_runner_orders AS (
+  SELECT order_id, runner_id,  
+CASE
+	WHEN pickup_time LIKE 'null' THEN ''
+	ELSE pickup_time
+	END AS pickup_time,
+CASE
+	WHEN distance LIKE 'null' THEN ''
+	WHEN distance LIKE '%km' THEN TRIM('km' from distance)
+	ELSE distance
+	END AS distance,
+CASE
+	WHEN duration LIKE 'null' THEN ''
+	WHEN duration LIKE '%mins' THEN TRIM('mins' from duration)
+	WHEN duration LIKE '%minute' THEN TRIM('minute' from duration)
+	WHEN duration LIKE '%minutes' THEN TRIM('minutes' from duration)
+	ELSE duration
+	END AS duration,
+CASE
+	WHEN cancellation IS NULL or cancellation LIKE 'null' THEN ''
+	ELSE cancellation
+	END AS cancellation
+FROM pizza_runner.runner_orders
+);
+SELECT * FROM updated_runner_orders;
+
+--Result:
+
++───────────+────────────+──────────────────────+───────────+───────────+──────────────────────────+
+| order_id  | runner_id  | pickup_time          | distance  | duration  | cancellation             |
++───────────+────────────+──────────────────────+───────────+───────────+──────────────────────────+
+| 1         | 1          | 2020-01-01 18:15:34  | 20        | 32        |                          |
+| 2         | 1          | 2020-01-01 19:10:54  | 20        | 27        |                          |
+| 3         | 1          | 2020-01-02 00:12:37  | 13.4      | 20        |                          |
+| 4         | 2          | 2020-01-04 13:53:03  | 23.4      | 40        |                          |
+| 5         | 3          | 2020-01-08 21:10:57  | 10        | 15        |                          |
+| 6         | 3          |                      |           |           | Restaurant Cancellation  |
+| 7         | 2          | 2020-01-08 21:30:45  | 25        | 25        |                          |
+| 8         | 2          | 2020-01-10 00:15:02  | 23.4      | 15        |                          |
+| 9         | 2          |                      |           |           | Customer Cancellation    |
+| 10        | 1          | 2020-01-11 18:50:20  | 10        | 10        |                          |
++───────────+────────────+──────────────────────+───────────+───────────+──────────────────────────+
