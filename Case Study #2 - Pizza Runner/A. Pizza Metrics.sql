3708225037a67ed03d32bd50b752cd4c40ce15f8
Subala Singh <90029373+subalasingh@users.noreply.github.com>
2022-05-27 11:07:32 +0530

    Update A. Pizza Metrics.sql

--- Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
+++ Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
@@ -103,3 +103,40 @@ FROM(
 +────────────+
 
 --7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT updated_customer_orders.customer_id,
+ SUM (CASE WHEN exclusions <> '' OR extras <> '' THEN 1 ELSE 0 END) AS changes,
+ SUM (CASE WHEN exclusions = '' AND extras = '' THEN 1 ELSE 0 END) AS no_change
+FROM updated_customer_orders JOIN updated_runner_orders
+ON updated_customer_orders.order_id = updated_runner_orders.order_id
+WHERE cancellation = ''
+GROUP BY updated_customer_orders.customer_id;
+
+--Result:
++──────────────+──────────+────────────+
+| customer_id  | changes  | no_change  |
++──────────────+──────────+────────────+
+| 101          | 0        | 2          |
+| 102          | 0        | 3          |
+| 103          | 3        | 0          |
+| 104          | 2        | 1          |
+| 105          | 1        | 0          |
++──────────────+──────────+────────────+
+
+--8. How many pizzas were delivered that had both exclusions and extras?
+
+SELECT 
+  SUM ( CASE WHEN exclusions <> '' AND extras <> '' THEN 1 ELSE 0 END )AS pizza_count
+FROM updated_customer_orders JOIN updated_runner_orders
+ON updated_customer_orders.order_id = updated_runner_orders.order_id
+WHERE cancellation = '';
+
+--Result:
++──────────────+
+| pizza_count  |
++──────────────+
+| 1            |
++──────────────+
+
+--9. What was the total volume of pizzas ordered for each hour of the day?
+
